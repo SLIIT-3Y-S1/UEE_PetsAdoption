2edb958946142bd5161fca04780ac7751d70e168
Ravindu Ishan <IT22358752@my.sliit.lk>
2024-10-07 14:49:46 +0530

    Refactor color constants and text styles - Added app color constants in the `colors.dart` file - Added new text styles in the `text_styles.dart` - Added Google Fonts dependency

---
+++ lib/core/constants/text_styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  static final TextStyle headlineLarge = GoogleFonts.fredoka(
+    textStyle: const TextStyle(
+      fontSize: 24.0,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+
+  static final TextStyle headlineMedium = GoogleFonts.fredoka(
+    textStyle: const TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+    ),
+  );
+
+  static final TextStyle labelMedium = GoogleFonts.fredoka(
+    textStyle: const TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w100
+    ),
+  );
+
+  static final TextStyle labelSmall = GoogleFonts.fredoka(
+    textStyle: const TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.w100,
+    ),
+  );
+
+  
+  static final TextStyle bodySmall = GoogleFonts.fredoka(
+    textStyle: const TextStyle(
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+    ),
+  );
+}
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:pawpal/core/constants/colors.dart';
-
+import 'package:pawpal/core/constants/text_styles.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
-    primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.background,
-    buttonTheme: const ButtonThemeData(
-      buttonColor: AppColors.primary,
-      textTheme: ButtonTextTheme.primary,
-    ),
-    textTheme: const TextTheme(
-      displayLarge: TextStyle(fontFamily: 'Roboto', fontSize: 32, fontWeight: FontWeight.bold, color: AppColors.textPrimary),
-      displayMedium: TextStyle(fontFamily: 'Roboto', fontSize: 28, fontWeight: FontWeight.bold, color: AppColors.textPrimary),
-      bodyLarge: TextStyle(fontFamily: 'Lato', fontSize: 16, fontWeight: FontWeight.normal, color: AppColors.textPrimary),
-      bodyMedium: TextStyle(fontFamily: 'Lato', fontSize: 14, fontWeight: FontWeight.normal, color: AppColors.textSecondary),
-      labelLarge: TextStyle(fontFamily: 'Roboto', fontSize: 16, fontWeight: FontWeight.bold, color: AppColors.buttonText),
+    
+    textTheme: TextTheme(
+      displayLarge: TextStyles.headlineLarge.copyWith(color: AppColors.textPrimary),
+      displayMedium: TextStyles.headlineMedium.copyWith(color: AppColors.textPrimary),
+      bodySmall: TextStyles.bodySmall.copyWith(color: AppColors.textSecondary),
+      labelMedium: TextStyles.labelMedium.copyWith(color: AppColors.textSecondary),
+      labelSmall: TextStyles.labelSmall.copyWith(color: AppColors.textSecondary),
     ),
     inputDecorationTheme: const InputDecorationTheme(
-      filled: true,
-      fillColor: AppColors.inputBackground,
-      border: OutlineInputBorder(),
-    ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: AppColors.accent).copyWith(surface: AppColors.background),
+      filled: false,
+      border: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ),
+      enabledBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.black),
+      ),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color: AppColors.accentYellow, width: 2.0),
+      ),
+    ), // textfields decorations
+    colorScheme: ColorScheme.fromSwatch()
+        .copyWith(surface: AppColors.background)
+        .copyWith(onError: AppColors.error),
   );
 }
