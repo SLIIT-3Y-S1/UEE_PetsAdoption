6d53b662ff1f5c84fc8e463cb951f2584ffd3b0c
Ravindu Ishan <IT22358752@my.sliit.lk>
2024-10-08 17:03:41 +0530

    changed home back to SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:pawpal/features/postings/screens/postings_screen.dart';
 import 'package:pawpal/features/splash/screens/splash_screen.dart';
+//import 'package:pawpal/features/vets/screens/vets_home_screen.dart';
 import 'package:pawpal/theme/theme.dart';
 
 void main() {
@@ -23,6 +24,6 @@ class MyApp extends StatelessWidget {
         title: 'PawPal',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.lightTheme,
-        home: const PostingsScreen());
+        home: const SplashScreen());
   }
 }
