11abc0f77bf24c1b2cad6319f79175667f0e4fd1
Raizurai <127380205+Ravindu-Ishan@users.noreply.github.com>
2024-10-11 10:55:20 +0530

    Revert "Feat 2 kasun"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:pawpal/features/discussions/screens/discussion_home_screen.dart';
+import 'package:pawpal/features/vets/screens/vets_home_screen.dart';
 
-void main() { 
+void main() {
   runApp(
     DevicePreview(
       enabled: true,
@@ -18,12 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
         title: 'PawPal',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 9, 8, 10)),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const DiscussionsHomeScreen());
+        home: const VetsHomeScreen());
   }
 }
