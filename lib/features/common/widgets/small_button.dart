0de19c56fd3dcf0c380e2c297b31f63f42c4f920
Ravindu Ishan <IT22358752@my.sliit.lk>
2024-10-11 16:14:01 +0530

    UI updates and posting system stucture and files Changlog: - Updated theme file - form decoration - Updated App Vectors - Updated HomeScreen to display different post type creation drawer (common drawer of everyone) - Fixed Navigation to profile and back to homescreen. Earlier it took you back to the login - Added small button - Added 2 more text styles

---
+++ lib/features/common/widgets/small_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:pawpal/core/constants/colors.dart';
+import 'package:pawpal/theme/theme.dart';
+
+class SmallButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  final VoidCallback onPressed;
+  final IconData? icon;
+
+  const SmallButton({
+    Key? key,
+    required this.color,
+    required this.text,
+    required this.onPressed,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        minimumSize: const Size(100, 50), // Set the size here
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null) Icon(icon, size: 20.0, color:AppColors.iconBlack),
+          if (icon != null) const SizedBox(width: 8.0),
+          Text(
+            text,
+            style: AppTheme.lightTheme.textTheme.bodyMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
